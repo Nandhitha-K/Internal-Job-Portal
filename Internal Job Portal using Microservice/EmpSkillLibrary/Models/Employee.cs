57aa7e6bde105cd61f5dec161a5d7dc348a17ed6
Nandhitha <nandhitha5454@gmail.com>
2024-07-29 15:02:12 +0530

    First commit4

---
+++ Internal Job Portal using Microservice/EmpSkillLibrary/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmpSkillLibrary.Models;
+
+public partial class Employee
+{
+    public string EmpId { get; set; } = null!;
+    public string? EmpName { get; set; }
+    public virtual ICollection<EmpSkill> EmpSkills { get; set; } = new List<EmpSkill>();
+}
